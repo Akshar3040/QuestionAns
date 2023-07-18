bf299a290fcd26f7d36355223436cf0c9fed10f1
AKSHAR LODHIA <akshar.lodhia@yudiz.com>
2023-07-18 11:09:43 +0530

    Final Completion

--- Assets/Script/QuestionGenerater.cs
+++ Assets/Script/QuestionGenerater.cs
@@ -6,27 +6,26 @@ using TMPro;
 public class QuestionGenerater : MonoBehaviour
 {
     public int NumberToFindRandomNumber;
-    public string Ans;
-   
+
+    string Ans;   
     string RandomNumber1;
     string RandomNumber2;
+    string signs;
 
     public TMP_Text DisplayOutPut;
     public TMP_Text RightOrWrong;
-
     public TMP_Text UserEnterFiled;
 
-    [SerializeField] private List<string> Signs = new List<string>();
-    public string signs;
     public Animator animator;
+    public ParticleSystem WinnerParticle;
+    [SerializeField] private List<string> Signs = new List<string>();
+    public Animator CharacterAnimation;
 
 
     private void Start()
     {
         RandomNumberGenerater();
     }
-    
-
 
     public void RandomNumberGenerater()
     {
@@ -36,23 +35,16 @@ public class QuestionGenerater : MonoBehaviour
         RandomNumber1 = Random.Range(0, NumberToFindRandomNumber).ToString("0");
         RandomNumber2 = Random.Range(0, NumberToFindRandomNumber).ToString("0");
 
-
-
         int sign = Random.Range(0, Signs.Count);
         signs = Signs[sign];
-
         DisplayOutPut.text = ("What is " + RandomNumber1 + "" + signs + "" + RandomNumber2 + " ?");
-        CheckSum();
-      
+        CheckSum();      
     }
-
-
 
     public void CheckSum()
     {
         int TempNumber1 = int.Parse(RandomNumber1);
         int TempNumber2 = int.Parse(RandomNumber2);
-
 
         if (signs == '*'.ToString())
         {
@@ -75,16 +67,18 @@ public class QuestionGenerater : MonoBehaviour
             Ans = (TempNumber1 / TempNumber2).ToString();
         }
     }
-
-
     public void Button(string value)
     {
+      
        var Checker = (UserEnterFiled.text += value.ToString());
+        
           
         if (Checker.Length == Ans.Length)
         {
             if (Checker == Ans)
             {
+                WinnerParticle.Play();
+                CharacterAnimation.SetTrigger("Winner");
                 RandomNumberGenerater();
                 StartCoroutine(ResertInputField1());
 
@@ -94,36 +88,26 @@ public class QuestionGenerater : MonoBehaviour
                 RightOrWrong.text = " Please Enter Correct Ans";
                 RightOrWrong.color = Color.red;
                 animator.SetTrigger("Ans");
+                CharacterAnimation.SetTrigger("Out");
                 StartCoroutine(ResertInputField());
-
-            }
-            
-                
+            }   
         }
-
     }
-
 
     IEnumerator ResertInputField()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(.8f);
         UserEnterFiled.text = "";
-
     }
     IEnumerator ResertInputField1()
     {
         yield return new WaitForSeconds(.5f);
         UserEnterFiled.text = "";
-
     }
 
     public void OnReset()
     {
         UserEnterFiled.text = "";
         RandomNumberGenerater();
-
     }
-
-
-
 }
